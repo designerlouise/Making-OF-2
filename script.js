9b6c7a142dd4f59ceae2f4968fba5aa051e7153c
designerlouise <designlouisegrafico@gmail.com>
2025-07-19 15:36:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Espaço reservado para animações JS, scroll suave ou interatividade
-// Exemplo: scroll suave para âncoras
-
+// Efeito scroll suave para links internos (se quiser adicionar âncoras)
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
     document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
+      behavior: "smooth"
     });
   });
 });
